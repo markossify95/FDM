85197b00612a78cdef16847ee1c973c5b14735a3
markossify95 <markoss95@hotmail.com>
2017-02-11 00:17:32 +0100

    Fixed minor profile bug

--- network/urls.py
+++ network/urls.py
@@ -6,7 +6,7 @@ from . import views
 # /network/
 urlpatterns = [
     url(r'^profiles/(?P<id>[0-9]+)/$', views.profile_view),
-    # url(r'^profiles/new/(?P<id>[0-9]+)/$', views.profile_view),
+    url(r'^profiles/new/(?P<id>[0-9]+)/$', views.profile_view),
     url(r'^search/$', views.filter_view),
     url(r'^login/$', views.login_view),
     url(r'^logout/$', views.logout_view),
